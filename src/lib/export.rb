794c1eda160d7d83154668f2356bb2779e5b6ecd
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-09-19 01:41:18 -0500

    Fixed file output

--- src/lib/export.rb
+++ src/lib/export.rb
@@ -33,13 +33,13 @@ module Hangouts
         def export()
             puts "Writing timestamp.txt"
             # int[10]
-            conv_file = File.write(@options.outdir + '/conversations.csv')
-            conv_file.puts google_to_date(@data['continuation_end_timestamp'][0...10])
+            conv_file = File.new(@options.outdir + '/conversations.csv', 'w')
+            conv_file.write google_to_date(@data['continuation_end_timestamp'][0...10])
 
             puts "Writing conversations.csv"
             # conv_id,people_ids
-            conv_file = File.write(@options.outdir + '/conversations.csv')
-            conv_file.puts "conv_id,people_ids"
+            conv_file = File.new(@options.outdir + '/conversations.csv', 'w')
+            conv_file.write "conv_id,people_ids"
             @data['conversation_state'].each do |conv|
                 conv_id = conv['conversation_id']['id']
                 people_ids = Set.new
@@ -47,14 +47,14 @@ module Hangouts
                     people_ids.add part['id']['chat_id']
                 end
                 people_ids = people_ids.to_a.join(':')
-                conv_file.puts '"' +conv_id + '","' + people_ids + '"'
+                conv_file.write '"' +conv_id + '","' + people_ids + '"'
             end
             conv_file.close()
 
             puts "Writing people.csv"
             # chat_id,gaia_id,name
             people = Set.new
-            people_file = File.write(@options.outdir + '/people.csv')
+            people_file = File.new(@options.outdir + '/people.csv', 'w')
             people_file.puts "chat_id,gaia_id,name"
             @data['conversation_state'].each do |conv|
                 conv['conversation_state']['conversation']['participant_data'].each do |part|
@@ -66,14 +66,14 @@ module Hangouts
                 end
             end
             people.to_a.each do |person|
-                people_file.puts '"' + person[:chat_id] + '","' + person[:gaia_id] + '","' + person[:name] + '"'
+                people_file.write '"' + person[:chat_id] + '","' + person[:gaia_id] + '","' + person[:name] + '"'
             end
             people_file.close()
 
             puts "Writing messages.csv"
             # conv_id,event_id,sender_id,message_id,segment_id,message_time,segment_type,segment_content
-            messages_file = File.write(@options.outdir + '/messages.csv')
-            messages_file.puts "conv_id,event_id,sender_id,message_id,segment_id,message_time,segment_type,segment_content"
+            messages_file = File.new(@options.outdir + '/messages.csv', 'w')
+            messages_file.write "conv_id,event_id,sender_id,message_id,segment_id,message_time,segment_type,segment_content"
             @data['conversation_state'].each do |conv|
                 conv_id = conv['conversation_id']['id']
                 if !conv['conversation_state'] .nil? and !conv['conversation_state']['event'].nil?
@@ -87,7 +87,7 @@ module Hangouts
                                 segment_type = segment['type']
                                 segment_content = segment['text']
                                 segment_id = index
-                                messages_file.puts '"' + conv_id.to_s + '","' + event_id.to_s + '","' + sender_id.to_s + '","' + message_id.to_s + '","' + segment_id.to_s + '","' + message_time.to_s + '","' + segment_type.to_s + '","' + segment_content.to_s.gsub('"', '""') + '"'
+                                messages_file.write '"' + conv_id.to_s + '","' + event_id.to_s + '","' + sender_id.to_s + '","' + message_id.to_s + '","' + segment_id.to_s + '","' + message_time.to_s + '","' + segment_type.to_s + '","' + segment_content.to_s.gsub('"', '""') + '"'
                             end
                         end
                     end
